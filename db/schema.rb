e6356b007b0d367eb5818d0c13fb9bf214893b79
Fred Araujo <fredericoaraujo@gmail.com>
2023-04-17 16:36:18 -0300

    Alteração no modelo de dados

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_091600) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_173615) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
+    t.string "author"
     t.text "body"
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
@@ -28,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_091600) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
   end
 
   add_foreign_key "comments", "posts"
